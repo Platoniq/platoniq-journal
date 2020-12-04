e86f1ab4bdbf098c0cf52838da26de37eb257e8d
Vera Rojman <vrojman@protonmail.com>
2020-12-04 10:37:01 +0100

    First stub with locomotive site

---
+++ app/assets/javascripts/sections/index.js
@@ -0,0 +1,11 @@
+// Import all the sections here
+//
+// Example:
+// export { default as MyAwesomeSection } from './my_awesome_section';
+//
+export { default as Hero }
+from './hero';
+export { default as LatestProject }
+from './latest_projects';
+export { default as Author }
+from './authors';
